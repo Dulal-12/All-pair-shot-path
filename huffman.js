a124a68374978dc81793b2f9a1b5a29c1838e3c7
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-01 21:04:35 +0600

    done huffman

--- huffman.js
+++ huffman.js
@@ -26,14 +26,45 @@ const huffman = (str)=>{
         return a.count - b.count;
        });
 
-    console.log(shortChar);
+    //console.log(shortChar);
+   let BinaryCode = [ ]
+ 
+  
+ 
+    for(let i = 0; i<shortChar.length ;i++){
+
+
+        if(i === 0 )
+        {
+            BinaryCode[i] = { char : shortChar[i].char , distance :  0};
+        }
+           
+        else{
+            var count = 0;
+            BinaryCode[i] = { char : shortChar[i].char , distance :  1};
+            count++;
+        }
+           
+       for(var j = i-1 ;j >= 0 ;j--)
+       {
+        BinaryCode[j] = { char : shortChar[j].char , distance :  BinaryCode[j].distance + 1};
+        //console.log(BinaryCode[j]);
+
+        
+       }
+         
+
+          
+
+              
+    }
 
     
 
-      
+      console.log(BinaryCode);
           
 }
 
 
-const str = 'AAAABBCCC';
+const str = 'AAAAdddddddbbbbbbbbcccccccc';
 huffman(str);
